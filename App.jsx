61c890dd397a6059e5d0171253bb4e9d64e161b2
DMN <102384385+DMNpy@users.noreply.github.com>
2024-01-25 21:23:50 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import DereceDonustur from './components/DereceDonustur'
+import SonucLabel from './components/SonucLabel'
+
+function App () {
+return (
+  <>
+  <DereceDonustur/>
+  <SonucLabel /> 
+  </>
+)
+}
+
+export default App
